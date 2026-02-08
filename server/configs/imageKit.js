6276f7306939642043e7adaf668e20445f785d0b
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2026-02-09 01:37:00 +0600

    splve image kit problem

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,7 +1,9 @@
-import ImageKit from '@imagekit/nodejs';
+import ImageKit from "@imagekit/nodejs";
 
 const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 export default imagekit;
