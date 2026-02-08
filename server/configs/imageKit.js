a28226eec838129aa3e973cda851a353caaa2a95
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2026-02-09 01:58:34 +0600

    update backend

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,9 +1,7 @@
-import ImageKit from "@imagekit/nodejs";
+import ImageKit from '@imagekit/nodejs';
 
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 export default imagekit;
